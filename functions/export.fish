02afb88954121bf0ebbfa6e42e20be727e3567fd
Pritam Kadam <phkadam2008@gmail.com>
2018-04-23 00:51:19 +0530

    fix setenv

--- functions/export.fish
+++ functions/export.fish
@@ -43,13 +43,21 @@ set -x LESS "-RSM~gIsw"
 
 # Colorful man pages
 # from http://pastie.org/pastes/206041/text
-setenv -x LESS_TERMCAP_mb (set_color -o red)
-setenv -x LESS_TERMCAP_md (set_color -o red)
-setenv -x LESS_TERMCAP_me (set_color normal)
-setenv -x LESS_TERMCAP_se (set_color normal)
-setenv -x LESS_TERMCAP_so (set_color -b blue -o yellow)
-setenv -x LESS_TERMCAP_ue (set_color normal)
-setenv -x LESS_TERMCAP_us (set_color -o green)
+# setenv -x LESS_TERMCAP_mb (set_color -o red)
+# setenv -x LESS_TERMCAP_md (set_color -o red)
+# setenv -x LESS_TERMCAP_me (set_color normal)
+# setenv -x LESS_TERMCAP_se (set_color normal)
+# setenv -x LESS_TERMCAP_so (set_color -b blue -o yellow)
+# setenv -x LESS_TERMCAP_ue (set_color normal)
+# setenv -x LESS_TERMCAP_us (set_color -o green)
+
+set -x LESS_TERMCAP_mb (printf "\033[01;31m")  
+set -x LESS_TERMCAP_md (printf "\033[01;31m")  
+set -x LESS_TERMCAP_me (printf "\033[0m")  
+set -x LESS_TERMCAP_se (printf "\033[0m")  
+set -x LESS_TERMCAP_so (printf "\033[01;44;33m")  
+set -x LESS_TERMCAP_ue (printf "\033[0m")  
+set -x LESS_TERMCAP_us (printf "\033[01;32m")  
 
 # grep colors
-setenv -x GREP_OPTIONS "--color=auto"
+# setenv -x GREP_OPTIONS "--color=auto"
